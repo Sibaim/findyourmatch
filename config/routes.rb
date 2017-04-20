3965674ed1048e3985da022f1f5d2624590525da
M Sibai <sibaim@gmail.com>
2017-04-20 14:07:00 +0200

    view list of users, /users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :users, only: [:index, :update, :show]
   resources :matches
-  root to: 'pages#home'
+  root to: "users#index"
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
