0ac41a7254ce9dc63bef3e0be1985d2a0964b702
M Sibai <sibaim@gmail.com>
2017-04-20 23:00:38 +0200

    Match model changed

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,3 +1,48 @@
 class Match < ApplicationRecord
-  has_and_belongs_to_many :users
+  belongs_to :first_student, :class_name => 'User', :foreign_key => 'first_student_id'
+  belongs_to :second_student, :class_name => 'User', :foreign_key => 'second_student_id'
+
+  def self.removeMatches(day)
+    #remove current matches for courseday
+    existing_matches = Match.where(day: day)
+
+    if existing_matches.length > 0
+      existing_matches.each do |match|
+        match.destroy
+      end
+    end
+  end
+
+  def self.couplesForCourseday(day)
+    day = day.to_date
+    couples = makeCouples()
+
+    couples.each do |student|
+      new_match = Match.create(first_student: student[0], second_student: student[1], day: day)
+    end
+  end
+
+  def self.makeCouples()
+    couples = []
+    picked_students = []
+    # query that returns an array with all students (no admins) as objects
+    students = User.all.select { |u| u.is_admin == false }
+    # add surrogate user to array if number of students is odd
+    students.push(User.find{ |u| u.no_teammate == true}) if students.length.odd?
+    # amount of couples per day is total students /2
+    couples_per_day = students.length / 2
+
+    couples_per_day.times do
+      randomizer_first = rand(students.length)
+      first_pick = students[randomizer_first]
+      picked_students << students.delete_at(randomizer_first)
+
+      randomizer_second = rand(students.length)
+      second_pick = students[randomizer_second]
+      picked_students << students.delete_at(randomizer_second)
+
+        couples << [first_pick, second_pick]
+      end
+    return couples
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+   has_many :match1st, :class_name => 'Match', :foreign_key => 'first_student_id'
+   has_many :match2nd, :class_name => 'Match', :foreign_key => 'second_student_id'
 
-  has_and_belongs_to_many :matches
-
-  def name
-   "#{first_name} #{last_name}"
- end
-
- def todays_match
-   matches.find{|hash| hash[:date] === Date.today}
- end
-
- def past_matches
-   matches.select{|hash| hash[:date] < Date.today}
- end
 end
