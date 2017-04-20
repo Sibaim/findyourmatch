8f26e68c4acf001f0cb3ab898079e30f4dad18ce
M Sibai <sibaim@gmail.com>
2017-04-20 12:07:22 +0200

    correct is_admin in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 User.delete_all
 Match.delete_all
 
-Mamdouh = User.create(first_name: "Mamdouh", last_name: "Sibai", admin: true, email: "sibaim@gmail.com", password: "abc1234")
-Jt = User.create(first_name: "Jt", last_name: "Breu", admin: false, email: "jt@gmail.com", password: "abc1234")
-Jack = User.create(first_name: "Jack", last_name: "Jonz", admin: false, email: "jack@gmail.com", password: "abc1234")
-Ces = User.create(first_name: "Ces", last_name: "Uygun",  admin: false, email: "ces@gmail.com", password: "abc1234")
-Florestan = User.create(first_name: "Florestan", last_name: "Korp",  admin: false, email: "florestan@gmail.com", password: "abc1234")
-Sera = User.create(first_name: "Sera", last_name: "Am", admin: false, email: "Sera@gmail.com", password: "abc1234")
+Mamdouh = User.create(first_name: "Mamdouh", last_name: "Sibai", is_admin: true, email: "sibaim@gmail.com", password: "abc1234")
+Jt = User.create(first_name: "Jt", last_name: "Breu", is_admin: false, email: "jt@gmail.com", password: "abc1234")
+Jack = User.create(first_name: "Jack", last_name: "Jonz", is_admin: false, email: "jack@gmail.com", password: "abc1234")
+Ces = User.create(first_name: "Ces", last_name: "Uygun",  is_admin: false, email: "ces@gmail.com", password: "abc1234")
+Florestan = User.create(first_name: "Florestan", last_name: "Korp",  is_admin: false, email: "florestan@gmail.com", password: "abc1234")
+Sera = User.create(first_name: "Sera", last_name: "Am", admin: false, is_admin: "Sera@gmail.com", password: "abc1234")
 
 
 
