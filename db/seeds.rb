823ba7f13541283207a9a72f4f9d6f1aa786fdae
M Sibai <sibaim@gmail.com>
2017-04-22 18:01:33 +0200

    bug fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,43 +4,51 @@ Match.delete_all
 User.create!(
   name: 'Mamdouh Sibai',
   email: 'sibaim@gmail.com',
-  password: 'abc1234',
+  password: 'asd1234',
   is_admin: true,
 )
-User.create!(
-  name: 'Sera Am',
-  email: 'sera@email.com',
-  password: 'abc1234',
-  is_admin: false,
-)
-User.create!(
-  name: 'JT Breu',
-  email: 'jt@email.com',
-  password: 'abc1234',
-  is_admin: false,
-)
-User.create!(
-  name: 'Ces Uygun',
-  email: 'ces@email.com',
-  password: 'abc1234',
-  is_admin: false,
-)
-User.create!(
-  name: 'Jack Jonz',
-  email: 'jack@email.com',
-  password: 'abc1234',
-  is_admin: false,
-)
-User.create!(
-  name: 'Bambo Borillo',
-  email: 'bambo@email.com',
-  password: 'abc1234',
-  is_admin: false,
-)
+# User.create!(
+#   name: 'Sera Am',
+#   email: 'sera@email.com',
+#   password: 'asd1234',
+#   is_admin: false,
+# )
+# User.create!(
+#   name: 'JT Breu',
+#   email: 'jt@email.com',
+#   password: 'asd1234',
+#   is_admin: false,
+# )
+# User.create!(
+#   name: 'Ces Uygun',
+#   email: 'ces@email.com',
+#   password: 'asd1234',
+#   is_admin: false,
+# )
+# User.create!(
+#   name: 'Jack Jonz',
+#   email: 'jack@email.com',
+#   password: 'asd1234',
+#   is_admin: false,
+# )
+# User.create!(
+#   name: 'Bambo Borillo',
+#   email: 'bambo@email.com',
+#   password: 'asd1234',
+#   is_admin: false,
+# )
 User.create!(
   name: 'I Have No Teammate',
   email: 'noemail@email.com',
-  password: 'abc1234',
+  password: 'asd1234',
   no_teammate: true,
-  is_admin: false,
+  is_admin: true,
 )
+
+25.times do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: 'asd1234'
+  )
+end
